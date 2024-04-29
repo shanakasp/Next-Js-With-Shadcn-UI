e3b16dcb3e486c6867c5b09b4c032052c496dec0
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-29 14:04:51 +0530

    modified page.tsx added card.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,49 @@
-export default function page() {
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
+interface Recipe {
+  title: string;
+  image: string;
+  time: number;
+  description: string;
+  vegan: boolean;
+  id: string;
+}
+
+async function getrecipe(): Promise<Recipe[]> {
+  const result = await fetch("http://localhost:4000/recipes");
+  return result.json();
+}
+
+export default async function Home() {
+  const recipes = await getrecipe;
   return (
     <div>
-      <main>Home</main>
+      <main>
+        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+          {recipes.map((recipe) => (
+            <Card key={recipe.id}>
+              <CardHeader>
+                <div>
+                  <CardTitle>{recipe.title}</CardTitle>
+                  <CardDescription>{recipe.time} minutes</CardDescription>
+                  <CardContent>{recipe.description}</CardContent>
+                  <CardFooter>
+                    <button>View Recipe</button>
+                    {recipe.vegan && <p>Vegan!</p>}
+                  </CardFooter>
+                </div>
+              </CardHeader>
+            </Card>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
