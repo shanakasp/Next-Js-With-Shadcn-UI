d8fa6a9bad61cbe0dbbabae2c581f5948f734a94
shanakasp <74164029+shanakasp@users.noreply.github.com>
2024-04-29 21:54:03 +0530

    modified page.tsx in app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,34 +16,46 @@ interface Recipe {
   id: string;
 }
 
-async function getrecipe(): Promise<Recipe[]> {
-  const result = await fetch("http://localhost:4000/recipes");
-  return result.json();
+async function getRecipes(): Promise<Recipe[]> {
+  try {
+    const response = await fetch("http://localhost:4000/recipes");
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch recipes");
+    }
+
+    const recipes: Recipe[] = await response.json();
+    return recipes;
+  } catch (error) {
+    console.error("Error fetching recipes:", error);
+    return []; // or handle the error accordingly
+  }
 }
 
 export default async function Home() {
-  const recipes = await getrecipe;
+  const recipes = await getRecipes();
+
   return (
-    <div>
-      <main>
-        <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-          {recipes.map((recipe) => (
-            <Card key={recipe.id}>
-              <CardHeader>
-                <div>
-                  <CardTitle>{recipe.title}</CardTitle>
-                  <CardDescription>{recipe.time} minutes</CardDescription>
-                  <CardContent>{recipe.description}</CardContent>
-                  <CardFooter>
-                    <button>View Recipe</button>
-                    {recipe.vegan && <p>Vegan!</p>}
-                  </CardFooter>
-                </div>
-              </CardHeader>
-            </Card>
-          ))}
-        </div>
-      </main>
-    </div>
+    <main>
+      <div className="grid grid-cols-3 gap-8">
+        {recipes.map((recipe) => (
+          <Card key={recipe.id} className="flex flex-col justify-between">
+            <CardHeader className="flex-row gap-4 items-center">
+              <div>
+                <CardTitle>{recipe.title}</CardTitle>
+                <CardDescription>{recipe.time} mins to cook.</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>
+              <p>{recipe.description}</p>
+            </CardContent>
+            <CardFooter className="flex justify-between">
+              <button>View Recipe</button>
+              {recipe.vegan && <p>Vegan!</p>}
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
   );
 }
